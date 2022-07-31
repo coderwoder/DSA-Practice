324e4b632cc476913cf72a61cf68458ff109ae61
coderwoder <shkmd29@gmail.com>
2022-08-01 00:14:54 +0530

    Added small comments

--- tempfile_inserbeg.cpp
+++ tempfile_inserbeg.cpp
@@ -5,32 +5,32 @@ class linkedlist
 {
     struct Node
     {
-        int data; // data | value
-        Node *next;
+        int data;   // data | value
+        Node *next; // address of the next node
     };
     Node *begin = NULL; // cur List is empty
 public:
     void insertbeg(int input)
     {
-        Node *temp = new Node; // new node creating
-        temp->data = input;    // val(input) stored in the data of temp
-        temp->next = NULL;     // initialized next to null
-        if (begin != NULL)
-            temp->next = begin;
-        begin = temp;
+        Node *temp = new Node;  // new node creating
+        temp->data = input;     // val(input) stored in the data of temp
+        temp->next = NULL;      // initialized next to null
+        if (begin != NULL)      // list not empty
+            temp->next = begin; // first node ko point karo(connecting new to 1)
+        begin = temp;           //        //begin ko point karwake isko first node banado
     }
-    void display()
+    void display() // displaying while you traverse
     {
         if (begin == NULL)
             cout << "Empty..." << endl;
         else
         {
-            Node *ptr = begin;
+            Node *ptr = begin; //          //starting the traverse from begin
             cout << "Displaying List : " << endl;
-            while (ptr != NULL)
+            while (ptr != NULL) //         //jab tak ptr end node(adrs=null) tak nahi ata
             {
-                cout << "\t" << ptr->data;
-                ptr = ptr->next;
+                cout << "\t" << ptr->data; // printing the data of the node that is pointed to
+                ptr = ptr->next;           // point to the adrs of the next node
             }
         }
     }
